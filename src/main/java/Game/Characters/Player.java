90fe8e9c2c42b442edf010f03fe23643f8bf412a
JMMoir <jmmoir@outlook.com>
2019-06-06 12:03:01 +0100

    weapons created

--- src/main/java/Game/Characters/Player.java
+++ src/main/java/Game/Characters/Player.java
@@ -1,18 +1,18 @@
 package Game.Characters;
-import Game.Weapon.Weapon;
+//import Game.Weapon.Weapon;
+//
+//import java.util.ArrayList;
+//
+//public class Player extends Character {
+////
+////    //private ArrayList<Loot> lootBag;
+////
+////    public Player(String name, int health) {
+////        super(name, health);
+////    }
+////
+////    public void addLoot(Loot loot){
+////        this.lootBag
+////    }
 
-import java.util.ArrayList;
-
-public class Player extends Character {
-
-    //private ArrayList<Loot> lootBag;
-
-    public Player(String name, int health) {
-        super(name, health);
-    }
-
-    public void addLoot(Loot loot){
-        this.lootBag
-    }
-
-}
+//}
--- src/main/java/Game/Weapon/Weapon.java
+++ src/main/java/Game/Weapon/Weapon.java
@@ -1,7 +1,39 @@
 package Game.Weapon;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Weapon {
 
+    WeaponEnum weapon;
+    ArrayList<WeaponEnum> list;
 
 
+    public Weapon() {
+        list = new ArrayList<WeaponEnum>();
+        weapon = getWeapon();
+
+    }
+
+
+    public WeaponEnum getWeapon(){
+        for(WeaponEnum weapon: WeaponEnum.values()){
+            list.add(weapon);
+        }
+        int random = (int)(Math.random() * list.size());
+        return list.get(random);
+
+    }
+
+    public String getName() {
+        return weapon.name();
+    }
+
+    public int getValue(){
+        return weapon.getValue();
+    }
+
+    public void setWeaponType(WeaponEnum weapon) {
+        this.weapon = weapon;
+    }
 }
